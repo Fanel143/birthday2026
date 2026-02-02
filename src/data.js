12caf733c749f7d8efaf810d71b0851af5f4fa54
Fanel143 <pkahur@gmail.com>
2026-02-02 13:30:30 +0530

    hello
    hehe

---
+++ src/data.js
@@ -0,0 +1,61 @@
+// ==========================
+// 1. Intro Screen
+// ==========================
+export const firstHeading = "A Cutiepie was born today, 29 years ago!"
+// Main heading text - Change this text as you like
+
+export const firstSubtext = "Yes, it’s YOU! A little surprise awaits..."
+// Subtext under heading (optional) - Change, leave blank or remove.
+
+
+// ==========================
+// 2. Cake Screen
+// ==========================
+// Add recipient's name here.
+export const NAME = "Cute Pratima !"
+
+
+// ==========================
+// 3. Photo Screen
+// ==========================
+export const photoScreenHeading = "Some Sweet Moments With You"
+// Heading above photos - Change this text as you like
+
+// Add your photos inside "public/images" folder
+// Example: { "/images/1.jpeg" }, Match the format and add more as needed
+export const photos = [
+    "/images/PRATIMA 1.jpg",
+    "/images/PRATIMA 2.jpg",
+    "/images/PRATIMA 5.jpeg",
+    "/images/PRATIMA 6.jpeg",
+    "/images/PRATIMA 7.jpeg",
+
+]
+
+
+// ==========================
+// 4. Message Screen
+// ==========================
+export const messageScreenHeading = "A Special Message"
+// Heading above card - Change this text as you like
+
+export const specialMessage = `Wish You Happy Birthday My Love When You Enters In My Life That Movement Is Unforgatable For Me That Day I'll Never Forget In My Life You Teach Me Everything I Am Very Thankfull to God Gives Me A Thats You , I Love You So Muchh My Princess 💗`
+// Main special message (you can edit freely)
+
+
+// ==========================
+// 4. Final Screen
+// ==========================
+export const finalScreenHeading = "One Last Thing..."
+// Heading above final surprise - Change this text as you like
+
+export const overlayText = "I LOVE YOU SOOOOO MUCH  ❤️"
+// First text inside the overlay card - Change this text as you like
+
+export const overlayMessage = "Once again, Happy Birthday! Hope you loved your surprise."
+
+// ==========================
+// Background Music
+// ==========================
+// Place your .mp3 file inside "public/audio" and update path below
+export const backgroundMusic = "./audio/bg.mp3"
